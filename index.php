c864c50cddc86a6b3e238f10a1079101117461a3
jerichogipoleo <jerichogipoleo@gmail.com>
2023-11-09 15:37:03 +0800

    IMAGES

--- index.php
+++ index.php
@@ -30,7 +30,13 @@
 		                </ol>
 		                <div class="carousel-inner">
 		                  <div class="item active">
-		                    <img src="images/banner1.png" alt="First slide">
+		                    <img src="images/HomepageCover.jpg" alt="First slide">
+		                  </div>
+						  <div class="item">
+		                    <img src="images/HomepageCover1.jpg" alt="First slide">
+		                  </div>
+						  <div class="item">
+		                    <img src="images/HomepageCover2.jpg" alt="First slide">
 		                  </div>
 		                </div>
 		                <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
